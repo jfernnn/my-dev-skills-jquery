438ce88de03927cb96c936bcb173afa73b348bf4
Joshua Fernelius <joshfern24@Joshuas-MacBook-Pro.local>
2020-05-13 10:49:05 -0600

    All functionality works

--- js/main.js
+++ js/main.js
@@ -1,25 +1,43 @@
+$('body').css('display', 'grid');
+$('body').css('width', '500px');
+$('body').css('height', '250px');
+$('body').css('padding', '50px 100px 0 100px');
+
 $('h1').css('text-align', 'center');
 $('h1').css('background-color', '#93C47D');
 $('h1').css('color', 'white');
 
+$('tbody').css('display', 'flex');
+$('tbody').css('flex-direction', 'column');
 $('tbody').css('text-align', 'center');
 
-$('tr').css('grid-template-columns', '1fr 8fr');
+//$('tr').css('grid-template-columns', '1fr 8fr');
+$('bar').css('display', 'flex')
+$('input').css('width', '300px');
+$('input').css('border-color', '#93C47D')
+$('button').css('border-color', '#93C47D')
+$('button').css('color', '#93C47D')
 
-$('button').css('background-color', '#E06666');
-$('button').css('color', 'white');
+$(`#addSkill`).on('click', function(e) {
+    var num = $(`#myInput`).val();
+    if(num !== ''){
+        console.log(num);
+        $('tbody').prepend(`<tr></tr>`);
+        $('tr').first().append(`<td><button>X</button></td>`);
+        $('tr').first().append(`<td>${num}</td>`);
+        $('button').first().css('background-color', '#E06666');
+        $('button').first().css('color', 'white');
+        $('td').css('color', 'orange');
+    }
+});
 
-$('td').css('color', 'orange');
-
-
-$('input').css('width', '500px');
-
-
-$('.btn-danger').on('click', function(e) {
+$(`tbody`).on('click', 'button', function(e) {
+    console.log('dsfds');
     $(this).closest('tr').remove();
 });
 
-$(`.addSkill`).on('click', function(e) {
-    const num = $("myText").val();
-    console.log(num);
-});
+
+
+
+
+
